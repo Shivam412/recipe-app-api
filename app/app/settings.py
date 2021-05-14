e3483e053083efdcdd01029fc80e69e562f4b8cf
Mohit <mohitrathore903@gmail.com>
2021-05-14 15:10:28 +0530

    Added endpoint for creating and authenticating users.

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'core'
+    'django.contrib.messages', 'django.contrib.staticfiles', 'rest_framework',
+    'rest_framework.authtoken', 'core', 'user'
 ]
 
 MIDDLEWARE = [
